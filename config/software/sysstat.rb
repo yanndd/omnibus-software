30716ac15324db3425578aa7fa29d1ef460587a0
Remi Hakim <remi@datadoghq.com>
2014-10-30 20:26:15 -0400

    First pass for build on mac os

--- config/software/sysstat.rb
+++ config/software/sysstat.rb
@@ -19,5 +19,4 @@ build do
     :env => env)
   command "make -j #{workers}", :env => {"LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
   command "sudo make install"
-  command "sudo chown -R vagrant #{install_dir}"
 end
